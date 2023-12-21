d2c4ce64a8da125bfb10af5d466f8593635b4d37
Elijah Melton <elimelt@uw.edu>
2023-12-21 12:33:27 -0800

    made better comments

--- scripts/replace.py
+++ scripts/replace.py
@@ -2,18 +2,18 @@ import argparse
 import subprocess
 
 def replace_chars(input_files, backup):
-    # Dictionary of characters to replace and their replacements
+    # characters to replace
     replacements = {
         '—': '-'
     }
 
-    # Generate the sed script
+    # generate sed script
     sed_script = []
     for old_char, new_char in replacements.items():
         sed_script.append(f"s/{old_char}/{new_char}/g")
 
-    # Write the sed commands to a temporary file
-    sed_file_path = 'sed_commands.txt'
+    # write to tpm file
+    sed_file_path = 'sed_commands.tpm'
     with open(sed_file_path, 'w') as f:
         f.write('\n'.join(sed_script))
 
@@ -21,20 +21,22 @@ def replace_chars(input_files, backup):
     for input_file in input_files:
         output_file = f"{input_file}.tmp"
         if backup:
-            # Create a backup of the original file
+            # backup original file
             subprocess.run(['cp', input_file, f"{input_file}.bak"])
 
-        # Use sed to perform replacements
+        # replacements with sed
         subprocess.run(['sed', '-f', sed_file_path, input_file], stdout=open(output_file, 'w'))
 
-        # Replace the original file with the output file
+        # replace orginal file
         subprocess.run(['mv', output_file, input_file])
 
-    # Clean up
+    # cleanup
     subprocess.run(['rm', sed_file_path])
 
     print("Replacement done.")
 
+
+# run with -b to create backup files
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Character replacement utility")
     parser.add_argument('input_files', nargs='+', help='List of input files')
